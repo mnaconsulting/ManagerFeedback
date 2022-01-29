4729136a706408cf281a8f505571bbd24394bac8
Nobiul <96243334+mnaconsulting@users.noreply.github.com>
2022-01-30 02:29:09 +0600

    EmployeeFeedback
    Employee Feedback by Manager

---
+++ story_content/6dQri96125i_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.60.26947.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:03.004%0D%0AToday!%20We%20will%20discuss%20about%20the%20Performance%20feedback.%0D%0A%0D%0A"}')
